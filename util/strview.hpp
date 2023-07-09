d9eaeae553cde6224e48bff30390126c0b741cac
Alexander Ripar <alexander.ripar@gmail.com>
2023-07-09 14:20:52 +0200

    Add hashed_strview
    This struct allows for the usage of pre-hashed strviews when a large
    number of comparisons is expected.

--- util/strview.hpp
+++ util/strview.hpp
@@ -4,6 +4,7 @@
 #include <cstdint>
 #include <cstring>
 
+#include "types.hpp"
 #include "strutil.hpp"
 
 struct strview
@@ -245,6 +246,78 @@ constexpr bool strstri(const strview& v1, const strview& v2) noexcept
 	}
 
 	return false;
+}
+
+struct hashed_strview
+{
+	const char8* m_beg;
+
+	u32 m_len;
+
+	u32 m_hash;
+
+	template<size_t Bytes>
+	static constexpr hashed_strview from_literal(const char (&lit)[Bytes]) noexcept
+	{
+		return hashed_strview{ lit, Bytes - 1, strhashc32(strview{ lit, Bytes - 1 }) };
+	}
+
+	constexpr hashed_strview() noexcept :
+		m_beg{ nullptr }, m_len{ 0 }, m_hash{ 0 } {}
+
+	constexpr hashed_strview(const char* beg, const char* end) noexcept :
+		m_beg{ beg }, m_len{ static_cast<u32>(end - beg) }, m_hash{ strhashc32(strview{ beg, end }) } {}
+
+	constexpr hashed_strview(const char* beg, u32 len) noexcept :
+		m_beg{ beg }, m_len{ len }, m_hash{ strhashc32(strview{ beg, len }) } {}
+
+	constexpr hashed_strview(const strview view) noexcept :
+		m_beg{ view.begin() }, m_len{ static_cast<u32>(view.len()) }, m_hash{ strhashc32(view) } {}
+
+	hashed_strview(const char* cstr) noexcept :
+		m_beg{ cstr }, m_len{ static_cast<u32>(strlen(cstr)) }, m_hash{ strhashc32(strview{ cstr, m_len }) } {}
+
+	constexpr strview to_strview() const noexcept
+	{
+		return strview{ m_beg, m_len };
+	}
+
+	constexpr size_t len() const noexcept
+	{
+		return m_len;
+	}
+
+	constexpr const char* begin() const noexcept
+	{
+		return m_beg;
+	}
+
+	constexpr const char* end() const noexcept
+	{
+		return m_beg + m_len;
+	}
+
+	constexpr char operator[](size_t n) const noexcept
+	{
+		return m_beg[n];
+	}
+
+	constexpr u32 hash() const noexcept
+	{
+		return m_hash;
+	}
+};
+
+constexpr bool streqc(const hashed_strview& v1, const hashed_strview& v2) noexcept
+{
+	if (v1.hash() != v2.hash() || v1.len() != v2.len())
+		return false;
+
+	for (size_t i = 0; i != v1.len(); ++i)
+		if (v1.m_beg[i] != v2.m_beg[i])
+			return false;
+
+	return true;
 }
 
 #endif // UTIL_STRVIEW_INCLUDE_GUARD
