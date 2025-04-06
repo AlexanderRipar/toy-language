4fddfcee776b40eaa0e978abaa8f538142e319cc
Alexander Ripar <alexander.ripar@gmail.com>
2025-04-06 15:42:09 +0200

    test: Improve command line parsing and add `--exit-with` and `--help` options
    The former is necessary to implement process-spawning tests.

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -38,6 +38,105 @@ static TimeDesc readable_time(u64 duration, u64 ticks_per_second) noexcept
 		return { "microseconds", static_cast<f64>(duration * 1'000'000) / ticks_per_second };
 }
 
+struct InvocationInfo
+{
+	bool ignore_debugbreaks;
+
+	bool show_help;
+
+	bool exit_with;
+
+	u32 exit_with_value;
+};
+
+static bool parse_uint(const char8* arg, u32* out) noexcept
+{
+	u32 value = 0;
+
+	if (*arg == '\0')
+		return false;
+
+	do
+	{
+		if (*arg < '0' || *arg > '9')
+			return false;
+
+		value = value * 10 + *arg - '0';
+
+		arg += 1;
+	}
+	while (*arg != '\0');
+
+	*out = value;
+
+	return true;
+}
+
+static bool parse_args(s32 argc, const char8** argv, InvocationInfo* out) noexcept
+{
+	memset(out, 0, sizeof(*out));
+
+	s32 arg_index = 1;
+
+	while (arg_index != argc)
+	{
+		if (strcmp(argv[arg_index], "--ignore-debugbreaks") == 0)
+		{
+			if (out->ignore_debugbreaks)
+			{
+				fprintf(stderr, "--ignore-debugbreaks specified more than once\n");
+
+				return false;
+			}
+
+			out->ignore_debugbreaks = true;
+
+			arg_index += 1;
+		}
+		else if (strcmp(argv[arg_index], "--exit-with") == 0)
+		{
+			if (out->exit_with)
+			{
+				fprintf(stderr, "--ignore-debugbreaks specified more than once\n");
+
+				return false;
+			}
+
+			if (arg_index + 1 == argc)
+			{
+				fprintf(stderr, "--exit-with expects an additional argument\n");
+
+				return false;
+			}
+
+			if (!parse_uint(argv[arg_index + 1], &out->exit_with_value))
+			{
+				fprintf(stderr, "--exit-with expects its additional argument to be a base-ten number\n");
+
+				return false;
+			}
+
+			out->exit_with = true;
+
+			arg_index += 2;
+		}
+		else if (strcmp(argv[arg_index], "--help") == 0 || strcmp(argv[arg_index], "-h") == 0)
+		{
+			out->show_help = true;
+
+			return true;
+		}
+		else
+		{
+			fprintf(stderr, "Unknown argument %s\n", argv[arg_index]);
+
+			return false;
+		}
+	}
+
+	return true;
+}
+
 s32 main(s32 argc, const char8** argv) noexcept
 {
 	if (minos::mem_reserve(65536) == nullptr)
@@ -45,21 +144,31 @@ s32 main(s32 argc, const char8** argv) noexcept
 
 	const u64 start = minos::exact_timestamp();
 
-	bool command_line_ok = argc == 1;
+	InvocationInfo invocation;
 
-	if (argc == 2 && strcmp(argv[1], "--ignore-debugbreaks") == 0)
+	if (!parse_args(argc, argv, &invocation))
 	{
-		g_ignore_debugbreaks = true;
-
-		command_line_ok = true;
-	}
-	
-	if (!command_line_ok)
-	{
-		fprintf(stderr, "Usage %s [--ignore-debugbreaks]\n", argv[0]);
+		fprintf(stderr, "Usage %s [--help | -h] [--ignore-debugbreaks] [--exit-with <N>]\n", argv[0]);
 
 		return EXIT_FAILURE;
 	}
+
+	if (invocation.show_help)
+	{
+		fprintf(stderr,
+			"This is the `comp` project's test suite. The following arguments are supported:\n"
+			"  --help | -h          - Show this message.\n"
+			"  --ignore-debugbreaks - Skip any debug break intrinsics triggered due to\n"
+			"                         failed tests. This should be enabled when running as\n"
+			"                         part of the test suite to avoid dumping core.\n"
+			"  --exit-with <N>      - Immediately exit with exit code <N>. This is used for\n"
+			"                         testing process spawning.\n");
+
+		return EXIT_SUCCESS;
+	}
+
+	if (invocation.exit_with)
+		return invocation.exit_with_value;
 
 	minos_tests();
 
