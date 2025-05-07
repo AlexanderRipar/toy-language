fad1e86771ae1ba8b7614f2444e239785a9bf529
Alexander Ripar <alexander.ripar@gmail.com>
2025-05-07 18:48:50 +0200

    print_type: Improve printing of function, integer and float types

--- diag/print_type.cpp
+++ diag/print_type.cpp
@@ -1,5 +1,19 @@
 #include "diag.hpp"
 
+static const char8* optional_tag_name(TypeTag tag) noexcept
+{
+	if (tag == TypeTag::Composite
+	 || tag == TypeTag::Func
+	 || tag == TypeTag::Array
+	 || tag == TypeTag::Slice
+	 || tag == TypeTag::Ptr
+	 || tag == TypeTag::Integer
+	 || tag == TypeTag::Float)
+		return "";
+
+	return tag_name(tag);
+}
+
 static void print_type_impl(diag::PrintContext* ctx, IdentifierPool* identifiers, TypePool* types, TypeId type_id, u32 indent, bool skip_initial_indent) noexcept
 {
 	if (type_id == TypeId::INVALID)
@@ -19,12 +33,15 @@ static void print_type_impl(diag::PrintContext* ctx, IdentifierPool* identifiers
 
 	const char8 name_closer = name_id == IdentifierId::INVALID ? '>' : '\"';
 
-	diag::buf_printf(ctx, "%*s%c%.*s%c %s",
+	const char8* tag_string = optional_tag_name(tag);
+
+	diag::buf_printf(ctx, "%*s%c%.*s%c%s%s",
 		skip_initial_indent ? 0 : indent * 2, "",
 		name_opener,
 		static_cast<s32>(name.count()), name.begin(),
 		name_closer,
-		tag_name(tag)
+		*tag_string == '\0' ? "" : " ",
+		tag_string
 	);
 
 	switch (tag)
@@ -33,8 +50,6 @@ static void print_type_impl(diag::PrintContext* ctx, IdentifierPool* identifiers
 	case TypeTag::Definition:
 	case TypeTag::CompInteger:
 	case TypeTag::CompFloat:
-	case TypeTag::Integer:
-	case TypeTag::Float:
 	case TypeTag::Boolean:
 	case TypeTag::Builtin:
 	case TypeTag::CompositeLiteral:
@@ -48,7 +63,17 @@ static void print_type_impl(diag::PrintContext* ctx, IdentifierPool* identifiers
 	{
 		diag::buf_printf(ctx, "\n");
 
-		break;
+		return;
+	}
+
+	case TypeTag::Integer:
+	case TypeTag::Float:
+	{
+		const NumericType* numeric_type = static_cast<const NumericType*>(simple_type_structure_from_id(types, type_id));
+
+		diag::buf_printf(ctx, " %s%u\n", tag == TypeTag::Integer ? numeric_type->is_signed ? "s" : "u" : "f", numeric_type->bits);
+
+		return;
 	}
 
 	case TypeTag::Slice:
@@ -69,11 +94,11 @@ static void print_type_impl(diag::PrintContext* ctx, IdentifierPool* identifiers
 		else
 			introducer = "*";
 
-		diag::buf_printf(ctx, " :: %s%s", introducer, reference->is_mut ? " mut " : "");
+		diag::buf_printf(ctx, " %s%s ", introducer, reference->is_mut ? " mut" : "");
 
 		print_type_impl(ctx, identifiers, types, reference->referenced_type_id, indent + 1, true);
 
-		break;
+		return;
 	}
 
 	case TypeTag::Array:
@@ -84,23 +109,34 @@ static void print_type_impl(diag::PrintContext* ctx, IdentifierPool* identifiers
 
 		print_type_impl(ctx, identifiers, types, array->element_type, indent + 1, true);
 
-		break;
+		return;
 	}
 
 	case TypeTag::Func:
-	{
-		// TODO
-
-		diag::buf_printf(ctx, "\n");
-
-		break;
-	}
-
 	case TypeTag::Composite:
 	{
-		diag::buf_printf(ctx, " :: {");
+		const FuncType* func_type;
 
-		MemberIterator it = members_of(types, type_id);
+		TypeId composite_type_id;
+
+		if (tag == TypeTag::Func)
+		{
+			func_type = static_cast<const FuncType*>(simple_type_structure_from_id(types, type_id));
+
+			composite_type_id = func_type->signature_type_id;
+		}
+		else
+		{
+			func_type = nullptr;
+
+			composite_type_id = type_id;
+		}
+
+		const TypeMetrics metrics = type_metrics_from_id(types, composite_type_id);
+
+		diag::buf_printf(ctx, " %s (sz=%" PRIu64 ", al=%" PRIu32 ", st=%" PRIu64 ") {", tag == TypeTag::Func ? "Func" : "Composite", metrics.size, metrics.align, metrics.stride);
+
+		MemberIterator it = members_of(types, composite_type_id);
 
 		bool has_members = false;
 
@@ -110,7 +146,7 @@ static void print_type_impl(diag::PrintContext* ctx, IdentifierPool* identifiers
 
 			const Range<char8> member_name = identifier_name_from_id(identifiers, member.name);
 
-			diag::buf_printf(ctx, "%s%*s%s%s%s%s\"%.*s\": ", has_members ? "" : "\n",
+			diag::buf_printf(ctx, "%s%*s%s%s%s%s\"%.*s\" ", has_members ? "" : "\n",
 				(indent + 1) * 2, "",
 				member.is_pub ? "pub " : "",
 				member.is_use ? "use " : "",
@@ -120,21 +156,25 @@ static void print_type_impl(diag::PrintContext* ctx, IdentifierPool* identifiers
 			);
 
 			if (!member.is_global)
-				diag::buf_printf(ctx, "@%" PRId64, member.offset);
+				diag::buf_printf(ctx, "(%+" PRId64 ") :: ", member.offset);
+			else
+				diag::buf_printf(ctx, ":: ");
 
 			print_type_impl(ctx, identifiers, types, member.type.complete, indent + 1, true);
 
 			has_members = true;
 		}
 
-		diag::buf_printf(ctx, "%*s}\n", has_members ? indent * 2 : 1, "");
+		diag::buf_printf(ctx, "%*s}%s", has_members ? indent * 2 : 1, "", tag == TypeTag::Func ? " -> " : "\n");
 
-		break;
+		if (tag == TypeTag::Func)
+			print_type_impl(ctx, identifiers, types, func_type->return_type_id, indent + 1, true);
+
+		return;
+	}
 	}
 
-	default:
-		ASSERT_UNREACHABLE;
-	}
+	ASSERT_UNREACHABLE;
 }
 
 void diag::print_type(minos::FileHandle out, IdentifierPool* identifiers, TypePool* types, TypeId type_id, const SourceLocation* source) noexcept
