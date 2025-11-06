cdfec93402c57448b7eb9aa3c51d698155acc760
Alexander Ripar <alexander.ripar@gmail.com>
2025-11-06 17:07:00 +0100

    common: Combine compiler-specific top-level `#if`s into one

--- infra/common.hpp
+++ infra/common.hpp
@@ -6,17 +6,25 @@
 #include <inttypes.h>
 
 #if defined(_MSC_VER) && !defined(__INTEL_COMPILER)
+	#include <intrin.h>
 	#define COMPILER_MSVC 1
 	#define COMPILER_NAME "msvc"
 	#define NORETURN __declspec(noreturn)
+	#define DEBUGBREAK __debugbreak()
 #elif defined(__clang__)
 	#define COMPILER_CLANG 1
 	#define COMPILER_NAME "clang"
 	#define NORETURN [[noreturn]]
+	#define DEBUGBREAK __builtin_debugtrap()
+	#if !__has_builtin(__builtin_debugtrap)
+		#error("Required __builtin_debugtrap not supported by used clang version")
+	#endif
 #elif defined(__GNUC__)
+	#include <signal.h>
 	#define COMPILER_GCC 1
 	#define COMPILER_NAME "gcc"
 	#define NORETURN __attribute__ ((noreturn))
+	#define DEBUGBREAK raise(SIGTRAP)
 #else
 	#error("Unsupported compiler")
 #endif
@@ -38,23 +46,6 @@ using char16 = wchar_t;
 
 using f32 = float;
 using f64 = double;
-
-#if COMPILER_MSVC
-	#include <intrin.h>
-	#define DEBUGBREAK __debugbreak()
-#elif COMPILER_CLANG
-	#if !__has_builtin(__builtin_debugtrap)
-		#error("Required __builtin_debugtrap not supported by used clang version")
-	#endif
-
-	#define DEBUGBREAK __builtin_debugtrap()
-#elif COMPILER_GCC
-	#include <signal.h>
-
-	#define DEBUGBREAK raise(SIGTRAP)
-#else
-	#error("Unknown compiler")
-#endif
 
 #ifdef NDEBUG
 	#define ASSERT_OR_IGNORE(x) do {} while (false)
