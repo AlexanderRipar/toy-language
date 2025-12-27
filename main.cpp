9962625252ce59afce4a87ef1f410777f408e5de
Alexander Ripar <alexander.ripar@gmail.com>
2025-12-27 19:01:46 +0100

    main: Factor out central code into `run_compilation` to avoid `goto`
    Note that the `goto FAILURE`s did not compile on gcc and clang in their old
    form, so this change was really necessary.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,24 @@
 #include <cstring>
 #include <cstdio>
 
+static bool run_compilation(CoreData* core) noexcept
+{
+	if (!import_prelude(core->interp, core->config->std.prelude.filepath))
+		return false;
+
+	const Maybe<TypeId> main_file_type_id = import_file(core->interp, core->config->entrypoint.filepath, false);
+
+	if (is_none(main_file_type_id))
+		return false;
+
+	const IdentifierId entrypoint_name = id_from_identifier(core->identifiers, core->config->entrypoint.symbol);
+
+	if (!evaluate_file_definition_by_name(core->interp, get(main_file_type_id), entrypoint_name))
+		return false;
+
+	return true;
+}
+
 s32 main(s32 argc, const char8** argv)
 {
 	if (argc == 0)
@@ -23,32 +41,22 @@ s32 main(s32 argc, const char8** argv)
 	{
 		CoreData core = create_core_data(range::from_cstring(argv[2]));
 
-		if (!import_prelude(core.interp, core.config->std.prelude.filepath))
-			goto FAILURE;
+		if (run_compilation(&core))
+		{
+			fprintf(stderr, "Success\n");
 
-		const Maybe<TypeId> main_file_type_id = import_file(core.interp, core.config->entrypoint.filepath, false);
+			release_core_data(&core);
 
-		if (is_none(main_file_type_id))
-			goto FAILURE;
+			return EXIT_SUCCESS;
+		}
+		else
+		{
+			print_errors(core.errors);
 
-		const IdentifierId entrypoint_name = id_from_identifier(core.identifiers, core.config->entrypoint.symbol);
+			release_core_data(&core);
 
-		if (!evaluate_file_definition_by_name(core.interp, get(main_file_type_id), entrypoint_name))
-			goto FAILURE;
-
-		release_core_data(&core);
-
-		fprintf(stderr, "Success\n");
-
-		return EXIT_SUCCESS;
-
-	FAILURE:
-
-		print_errors(core.errors);
-
-		release_core_data(&core);
-
-		return EXIT_FAILURE;
+			return EXIT_FAILURE;
+		}
 	}
 	else
 	{
