866c36f089928a292736e7db9c9f1dd1e7f8bfe8
Alexander Ripar <alexander.ripar@gmail.com>
2026-03-16 18:56:18 +0100

    template_helpers: Remove unused `using const_ptr_type` definitions

--- infra/template_helpers.hpp
+++ infra/template_helpers.hpp
@@ -17,37 +17,4 @@ constexpr bool is_ptr_type<T* volatile> = true;
 template<typename T>
 constexpr bool is_ptr_type<T* const volatile> = true;
 
-template<typename T>
-struct ConstexprPtrTypeHelper
-{
-	using type = void;
-};
-
-template<typename T>
-struct ConstexprPtrTypeHelper<T*>
-{
-	using type = const T*;
-};
-
-template<typename T>
-struct ConstexprPtrTypeHelper<const T*>
-{
-	using type = const T*;
-};
-
-template<typename T>
-struct ConstexprPtrTypeHelper<volatile T*>
-{
-	using type = const volatile T*;
-};
-
-template<typename T>
-struct ConstexprPtrTypeHelper<const volatile T*>
-{
-	using type = const volatile T*;
-};
-
-template<typename T>
-using const_ptr_type = typename ConstexprPtrTypeHelper<T>::type;
-
 #endif // TEMPLATE_HELPERS_INCLUDE_GUARD
